92396c3dfa1580adde868b457a20851a0e6efc22
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-02 12:39:15 +0000

    —-

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RecipeApp/",
-  plugins: [react()],
+  base: "/Cookify/",
+  plugins: [react(),
+  VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Cookify Recipe App',
+        short_name: 'Cookify',
+        description: 'A recipe app using themealdb api',
+        background_color: 'yellow', 
+        display: 'standalone', 
+        start_url: '/Cookify',
+        scope: '/',
+        orientation: 'portrait',
+        theme_color: 'yellow',
+        icons: [
+		
+	]
+      }
+    })
+  ],
   assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
 })
