038e22e351610dbe76123d490af53369d16787a4
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-04 16:45:59 +0000

    —-

--- src/Components/Food.jsx
+++ src/Components/Food.jsx
@@ -19,7 +19,7 @@ function Food(f){
             <button className="bg-black text-white w-[200px] mx-auto shadow-[1px_2px_3px_3px_rgba(0,0,0,0.5)] rounded-lg text-black active:shadow-none text-[1.5em] py-2" onClick={async ()=>
                 {setShow(true); setData(await fetchFood(f.id, setLoading, setIngredients)); }}>Get Recipe</button>
             <div className={show ? ((loading ? "flex justify-center items-center ":"") +"visible fixed w-[90vw] md:w-[50vw] h-[90vh] backdrop-blur-2xl border-[var(--primary-color)] border-[2px] rounded-lg p-[20px] z-10 transition duration-1000 overflow-hidden overflow-y-scroll selfScroll top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]") : "hidden"}>
-                {loading&&<AiOutlineLoading className="flex mx-auto p-1 bg-gray-100 fill-yellow-400 text-[3em] md:text-[5em] roll rounded-full"/>}
+                {loading&&<AiOutlineLoading className="flex mx-auto p-1 bg-gray-100 fill-black text-[3em] md:text-[5em] roll rounded-full"/>}
 
                 {!loading&&<FoodRecipe
                     showDialog={closeDialog}
